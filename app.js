de5277d5147758f5fc984a31e9fc33f36c57540a
unknown <1007700289@u.icesi.edu.co>
2024-05-29 10:46:29 -0500

    style: changes in css and html

--- app.js
+++ app.js
@@ -28,10 +28,50 @@ const users = [
 let products = [
     {
         id: 1,
-        description: "Guayos adidas edición CR7",
-        name: "Guayos adidas",
+        description: "Sombrero rojo redondeado",
+        name: "Sombrero",
         price: 50,
-        image: "resources/product-1.jpg",
+        image: "https://dcdn.mitiendanube.com/stores/003/156/683/products/1f416b9764a0084c8b9156b4d89a3b9d-faface75292c698d1217150348644797-1024-1024.webp",
+        stock: 50
+    },
+    {
+        id: 2,
+        description: "Gran abrigo de buena calidad",
+        name: "Abrigo de manga larga",
+        price: 20,
+        image: "https://static.zara.net/assets/public/f314/cdc5/9dbb414e9f26/b7365d9dfd40/02531118800-016-p/02531118800-016-p.jpg?ts=1709199206424&w=563",
+        stock: 10
+    },
+    {
+        id: 3,
+        description: "Color rojo",
+        name: "Vestido",
+        price: 11,
+        image: "https://static.zara.net/assets/public/a4f2/ecfe/1553465d8324/a8c11aea3760/05919811600-1-p/05919811600-1-p.jpg?ts=1716553907414&w=563",
+        stock: 5
+    },
+    {
+        id: 4,
+        description: "Blusa de escote pico con volantes y manga larga",
+        name: "Blusa volantes",
+        price: 25,
+        image: "https://static.zara.net/assets/public/285a/02f7/d98f499580a0/54503ac8649a/03666077403-p/03666077403-p.jpg?ts=1716372279679&w=563",
+        stock: 15
+    },
+    {
+        id: 5,
+        description: "Falda midi confeccionada en tejido satinado.",
+        name: "Falda midi",
+        price: 30,
+        image: "https://static.zara.net/assets/public/6c24/a618/def3433686fa/d0237ecc7d96/08338501051-p/08338501051-p.jpg?ts=1716478199837&w=563",
+        stock: 10
+    },
+    {
+        id: 6,
+        description: "Top corto de escote pico y tirantes finos con lazos.",
+        name: "Top crop",
+        price: 20,
+        image: "https://static.zara.net/assets/public/82b6/8002/2e7c45419a9f/406f8d2aa76e/04331025712-p/04331025712-p.jpg?ts=1716378964082&w=563",
         stock: 50
     }
 ];
